60d230bedf265364d18c59eeb8d3904ecb244df0
cjdaly <cjdaly@locosoft.net>
2018-01-26 18:49:15 -0800

    staging changes

---
+++ setup/sudo-setup-rock64.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+####
+# Copyright (c) 2018 Chris J Daly (github user cjdaly)
+# 
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+# 
+# SPDX-License-Identifier: EPL-2.0
+####
+
+echo "Configuring Java repo ..."
+add-apt-repository -y ppa:webupd8team/java
+echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
+
+echo "Configuring Neo4j repo ..."
+wget -O - https://debian.neo4j.org/neotechnology.gpg.key | apt-key add -
+echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.list
+
+echo "Update, Upgrade, Install ..."
+apt-get update
+apt-get upgrade -y
+apt-get install -y oracle-java8-installer python-pip neo4j=3.1.4
+
+echo "Neo4j post-install config ..."
+apt-mark hold neo4j
+sudo -u neo4j neo4j-admin set-initial-password cascade
+
+echo "Blinkstick setup ..."
+pip install setuptools
+pip install blinkstick
+blinkstick --add-udev-rule
