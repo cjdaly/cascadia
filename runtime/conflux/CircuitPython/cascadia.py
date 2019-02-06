cb4e6cbab310731fe1225cc9680fa2e302a1efe1
cjdaly <cjdaly@locosoft.net>
2019-02-06 00:03:54 -0800

    sneaky snacker

--- runtime/conflux/CircuitPython/cascadia.py
+++ runtime/conflux/CircuitPython/cascadia.py
@@ -10,16 +10,22 @@
 
 import os, board
 
-def RGB0_init():
+def init():
   machine=os.uname().machine
   if "Gemma" in machine:
     import adafruit_dotstar as dotstar
     global DS_0
     DS_0 = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.5)
-  elif ("HalloWing" in machine) or ("Trellis" in machine):
+  elif "HalloWing" in machine:
     import neopixel
     global NP_0
     NP_0 = neopixel.NeoPixel(board.NEOPIXEL,1,brightness=0.5)
+#    global NP_Strip
+#    NP_Strip = neopixel.NeoPixel(board.EXTERNAL_NEOPIXEL, 20, brightness=0.5)
+  elif "Trellis" in machine:
+    import neopixel
+    global NP_0
+    NP_0 = neopixel.NeoPixel(board.NEOPIXEL,32,brightness=0.5)
 
 def RGB0_set(r,g,b):
   machine=os.uname().machine
@@ -30,3 +36,16 @@ def RGB0_set(r,g,b):
     global NP_0
     NP_0[0]=(r,g,b)
 
+def RGB_fill(r,g,b):
+  machine=os.uname().machine
+  if "Gemma" in machine:
+    RGB0_set(r,g,b)
+  elif "HalloWing" in machine:
+    if 'NP_Strip' in globals():
+      global NP_Strip
+      NP_Strip.fill(r,g,b)
+    else:
+      RGB0_set(r,g,b)
+  elif "Trellis" in machine:
+    global NP_0
+    NP_0.fill(r,g,b)
