17f6c317639faf18e02d3f342e4f4718964c7c18
cjdaly <cjdaly@locosoft.net>
2019-02-17 19:57:34 -0800

    just a test

--- source/net.locosoft.cascadia.conflux.CircuitPython/src/net/locosoft/cascadia/conflux/CircuitPython/internal/CircuitPythonReader.java
+++ source/net.locosoft.cascadia.conflux.CircuitPython/src/net/locosoft/cascadia/conflux/CircuitPython/internal/CircuitPythonReader.java
@@ -70,7 +70,7 @@ public class CircuitPythonReader extends Cascade {
 	}
 
 	protected Drop spill(Id context) throws Exception {
-		if (thisId(context)) {
+		if (thisId(context) && _reader.ready()) {
 			String line = _reader.readLine();
 			if ((line != null) && !line.trim().isEmpty()) {
 				return new StringDrop(line);
